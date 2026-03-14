4fb740d1ca33e20a5a7860ecaf640df3f375bfa2
joyti299 <mjoyti2004@gmail.com>
2026-03-15 07:52:05 +0800

    correction of curtain part removal

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const playBtn = document.querySelector(".play-btn");
-const curtain = document.getElementById("curtain");
 
 playBtn.addEventListener("click", () => {
-
-  // open curtain
-  curtain.classList.add("curtain-open");
-
-  // go to cake page after animation
   setTimeout(() => {
     window.location.href = "cake.html";
-  }, 1500);
-
+  }, 500);
 });
